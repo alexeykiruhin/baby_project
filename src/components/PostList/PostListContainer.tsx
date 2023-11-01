3c740400286ca72cf7e7e9bbfd72852eed2de8ac
alexeykiruhin <q23sa3501080@gmail.com>
2023-11-01 17:00:31 +0300

    Change url api

--- src/components/PostList/PostListContainer.tsx
+++ src/components/PostList/PostListContainer.tsx
@@ -2,11 +2,19 @@ import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
 import {fetchPosts} from '../../redux/slices/home';
 import PostListComponent from './PostListComponent';
+import axios from 'axios';
 
 const PostListContainer: React.FC = () => {
     const posts = useAppSelector(state => state.home.posts)
     const dispatch = useAppDispatch();
     useEffect(() => {
+        axios.get('http://45.142.36.60:5000/api/time', {
+                    withCredentials: true,
+                    // headers: {'Authorization': `Bearer ${localStorage.getItem('refresh_token')}`}
+                }).then((response) => {
+                    console.log('api/time', response.data);
+                    return response.data
+                })
         dispatch(fetchPosts());
     }, [dispatch])
 
