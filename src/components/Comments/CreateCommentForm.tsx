7d21ec0ee34320b665d0838b98a0699815e6d20c
alexeykiruhin <q23sa3501080@gmail.com>
2024-02-21 14:51:23 +0300

    Add scroll to comment

--- src/components/Comments/CreateCommentForm.tsx
+++ src/components/Comments/CreateCommentForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Button, Form, Input} from 'antd';
 import styles from "./CreateComment.module.css";
 import Title from "antd/es/typography/Title";
@@ -10,21 +10,28 @@ const {TextArea} = Input;
 
 const CreateCommentForm: React.FC<CreateCommentType> = ({onFinish, onUpload, AddComment}) => {
     const [form] = Form.useForm();
+    const titleRef = useRef<HTMLHeadingElement>(null); // Создание ref
 
     const handleCancelComment = () => {
         AddComment(false)
-    }
 
+    }
     const sendCommentText = (obj: { Text: string }) => {
         let text = obj.Text;
         onFinish(text);
         // очистка поля ввода комментария
         form.resetFields()
+
     }
 
+    useEffect(() => {
+        // При загрузке компонента прокручиваем страницу до компонента Title
+        titleRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, []);
+
     return (
-        <div className={styles.CreateComment}>
-            <Title level={3} style={{margin: '20px 0'}}>
+        <div className={styles.CreateComment} ref={titleRef}>
+            <Title level={3} style={{margin: '20px 0'}} >
                 {/*<a href={#comment}>Add comment</a> // сделать якорь, при нажатии на add comment перемещение к форме*/}
                 Add comment
             </Title>
--- src/components/SinglePostView/SinglePostViewContainer.tsx
+++ src/components/SinglePostView/SinglePostViewContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import {getPost} from "../../redux/slices/post";
 import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 import {useParams} from "react-router-dom";
@@ -12,11 +12,16 @@ const SinglePostViewContainer: React.FC = () => {
     const post = useAppSelector((state) => state.post.post)
     const dispatch = useAppDispatch()
     const paramPostId = useParams();
+    const commentsRef = useRef<HTMLHeadingElement>(null); // Создание ref
+
     console.log('paramPostId', paramPostId.postId)
     useEffect(() => {
         dispatch(getPost(paramPostId))
     }, [dispatch, paramPostId]);
 
+    useEffect(() => {
+        commentsRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, []);
     return (
         <div
             style={{display: 'flex', marginTop: '73px', flexDirection: 'column', alignItems: 'center'}}
@@ -27,7 +32,7 @@ const SinglePostViewContainer: React.FC = () => {
                 {/*попробовать заменить Space на обычный див и его настроить так как надо*/}
                 {/*есть отступ сверху, можно его чем то заполнить*/}
                 <PostViewComponent post={post}/>
-                <Title level={2} style={{textAlign: 'center', marginBottom: '0.83em'}}>Comments</Title>
+                <Title level={2} style={{textAlign: 'center', marginBottom: '0.83em'}} ref={commentsRef}>Comments</Title>
                 <CommentsContainer postId={paramPostId.postId}/>
             </div>
         </div>
